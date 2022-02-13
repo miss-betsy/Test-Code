b78dda2ee9363e9ef10a162313ddd15de57cff43
miss-betsy <99562716+miss-betsy@users.noreply.github.com>
2022-02-13 14:07:45 -0500

    added rev color sensor sample code for RGB values

--- 2022 Robot/src/main/java/frc/robot/Robot.java
+++ 2022 Robot/src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 //import edu.wpi.first.wpilibj.Controller;
 //import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.I2C;
@@ -119,9 +120,50 @@ public class Robot extends TimedRobot {
    * SmartDashboard integrated updating.
    */
   @Override
-  public void robotPeriodic() {}
-  
+  public void robotPeriodic() {
+            
+          //Color Sensor
+              /**
+              * The method GetColor() returns a normalized color value from the sensor and can be
+              * useful if outputting the color to an RGB LED or similar. To
+              * read the raw color, use GetRawColor().
+              * 
+              * The color sensor works best when within a few inches from an object in
+              * well lit conditions (the built in LED is a big help here!). The farther
+              * an object is the more light from the surroundings will bleed into the 
+              * measurements and make it difficult to accurately determine its color.
+              */
+              Color detectedColor = m_colorSensor.getColor();
 
+              /**
+              * The sensor returns a raw IR value of the infrared light detected.
+              */
+              double IR = m_colorSensor.getIR();
+
+              /**
+              * Open Smart Dashboard or Shuffleboard to see the color detected by the 
+              * sensor.
+              */
+              SmartDashboard.putNumber("Red", detectedColor.red);
+              SmartDashboard.putNumber("Green", detectedColor.green);
+              SmartDashboard.putNumber("Blue", detectedColor.blue);
+              SmartDashboard.putNumber("IR", IR);
+
+              /**
+              * In addition to RGB IR values, the color sensor can also return an 
+              * infrared proximity value. The chip contains an IR led which will emit
+              * IR pulses and measure the intensity of the return. When an object is 
+              * close the value of the proximity will be large (max 2047 with default
+              * settings) and will approach zero when the object is far away.
+              * 
+              * Proximity can be used to roughly approximate the distance of an object
+              * or provide a threshold for when an object is close enough to provide
+              * accurate color values.
+              */
+              int proximity = m_colorSensor.getProximity();
+
+              SmartDashboard.putNumber("Proximity", proximity);
+        }
   /**
    * This autonomous (along with the chooser code above) shows how to select between different
    * autonomous modes using the dashboard. The sendable chooser code works with the Java
