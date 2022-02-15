1e46d7cdff75fa0bd67809d883251b1685dddf2c
miss-betsy <99562716+miss-betsy@users.noreply.github.com>
2022-02-14 22:14:23 -0500

    added spark max and beam breaks to conveyor subsystem

--- 7198-2022_code/src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
+++ 7198-2022_code/src/main/java/frc/robot/subsystems/ConveyorSubsystem.java
@@ -4,11 +4,29 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ConveyorSubsystem extends SubsystemBase {
   /** Creates a new ConveyorSubsystem. */
-  public ConveyorSubsystem() {}
+  public ConveyorSubsystem() {
+
+    //Inside Robot
+      CANSparkMax conveyorMotor = new CANSparkMax(3, MotorType.kBrushless);
+
+    //Sensors:
+      //In Gate: Beam Break
+        DigitalInput inGate_BB = new DigitalInput(1);
+      //Mid Gate: Beam Break
+        DigitalInput midGate_BB = new DigitalInput(2);
+      //Shooter Gate: Beam Break
+        DigitalInput shooter_BB = new DigitalInput(3);
+      
+
+  }
 
   @Override
   public void periodic() {
