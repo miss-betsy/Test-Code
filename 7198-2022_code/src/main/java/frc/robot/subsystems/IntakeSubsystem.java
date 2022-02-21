f8c209e35ad9a1870f7e5e2c5f1dac7cdaca933f
miss-betsy <99562716+miss-betsy@users.noreply.github.com>
2022-02-20 22:43:59 -0500

    intake - set rollermotor power

--- 7198-2022_code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ 7198-2022_code/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -10,7 +10,9 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.CAN;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.StopIntake;
 
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
@@ -24,14 +26,36 @@ public class IntakeSubsystem extends SubsystemBase {
     //Roller Drive
       rollerMotor = new CANSparkMax(1, MotorType.kBrushless);
 
-      
-
     //Pivot - Air
       intakePneumatic = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 1);
+
+          //Default command for the subsystem
+          this.setDefaultCommand(new StopIntake(this));   
   }
 
-  public void setRollerPower(double power){
-    rollerMotor.set(power);
+  //Control motor speed
+  public void set(final double s) {
+    rollerMotor.set(s);
+  }
+  public void intake(){
+    set(1.0);
+  }
+  public void eject(){
+    set(-1.0);
+  }
+
+  public void stop(){
+    rollerMotor.stopMotor();
+  }
+
+
+  //Control the pneumatic solenoid
+  public void raiseIntake(){
+    intakePneumatic.set(Value.kReverse);
+  }
+
+  public void lowerIntake(){
+    intakePneumatic.set(Value.kForward);
   }
 
   @Override
