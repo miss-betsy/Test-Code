93b33b82fcbe73f5836bc7391c7bd18b767fdaf3
miss-betsy <99562716+miss-betsy@users.noreply.github.com>
2022-03-12 10:37:11 -0500

    removed 'hi'

--- 2022 Robot/src/main/java/frc/robot/Robot.java
+++ 2022 Robot/src/main/java/frc/robot/Robot.java
@@ -38,7 +38,6 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 //import edu.wpi.first.util.sendable.SendableRegistry;
 
-//hi
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
